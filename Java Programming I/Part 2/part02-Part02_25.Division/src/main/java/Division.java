00e1c1729bc6a155a31cacac8afccd8e20171efb
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-06-28 03:55:54 +0300

    Division

---
+++ Java Programming I/Part 2/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,14 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+        
+        division(7,2);
+    }
+
+    public static void division(int fnum, int snum){
+        double result = (double)fnum/snum;
+        System.out.println(result);
+    }
+}
