db7a4e79e9e8b09eb4fa86c73ccdb1e12e7be2d2
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-06-28 18:40:09 +0300

    StarSign

--- Java Programming I/Part 2/part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ Java Programming I/Part 2/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -14,23 +14,33 @@ public class StarSign {
         System.out.println("\n---");
         printRectangle(5, 6);
         System.out.println("\n---");
-        printTriangle(3);
+        printTriangle(9);
         System.out.println("\n---");
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+        for(int i=0; i<number;i++){
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+        for(int i=0; i<size; i++){
+            printStars(size);
+        }
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        for(int i=0; i<height; i++){
+            printStars(width);
+        }
+        
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+        }
     }
 }
