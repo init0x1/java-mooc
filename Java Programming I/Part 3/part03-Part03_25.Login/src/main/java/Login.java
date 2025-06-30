df94fa68292ae69e765257aef676f10451f78543
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-06-30 07:48:07 +0300

    LineByLine

--- Java Programming I/Part 3/part03-Part03_25.Login/src/main/java/Login.java
+++ Java Programming I/Part 3/part03-Part03_25.Login/src/main/java/Login.java
@@ -5,21 +5,21 @@ public class Login {
 
     public static void main(String[] args) {
 
-        String[] user1 = {"alex","sunshine"};
-        String[] user2 = {"emma","haskell"};
+        String[] user1 = {"alex", "sunshine"};
+        String[] user2 = {"emma", "haskell"};
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter username: ");
         String userName = scanner.nextLine();
         System.out.println("Enter password: ");
         String password = scanner.nextLine();
-        
-        if(userName.equals(user1[0]) && password.equals(user1[1])
-                || userName.equals(user2[0]) && password.equals(user2[1])){
+
+        if (userName.equals(user1[0]) && password.equals(user1[1])
+                || userName.equals(user2[0]) && password.equals(user2[1])) {
             System.out.println("You have successfully logged in!");
-        }else{
-            System.out.println("Incorrect username or password!");        
+        } else {
+            System.out.println("Incorrect username or password!");
         }
-      
+
     }
 }
