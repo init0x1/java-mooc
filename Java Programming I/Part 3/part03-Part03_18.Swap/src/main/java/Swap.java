6c09934270023ec8b5971a00137a104ebe5c7c78
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-06-30 06:20:12 +0300

    swapArea

---
+++ Java Programming I/Part 3/part03-Part03_18.Swap/src/main/java/Swap.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        System.out.println("Give two indices to swap: ");
+        int firstIndex = Integer.valueOf(scanner.nextLine());
+        int secIndex = Integer.valueOf(scanner.nextLine());
+        
+        int swapArea = array[firstIndex];
+        array[firstIndex] = array[secIndex];
+        array[secIndex] = swapArea;
+
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
