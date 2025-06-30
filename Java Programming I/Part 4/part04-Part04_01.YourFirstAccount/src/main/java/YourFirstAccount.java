aea5e3b291356f18858e86c8141dad9fd226a188
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-06-30 14:29:07 +0300

    Your first account

---
+++ Java Programming I/Part 4/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,9 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        Account init0x1 = new Account("Abdelrahman Ali",100.0);
+        init0x1.deposit(20.0);
+        System.out.println(init0x1);
+    }
+}
